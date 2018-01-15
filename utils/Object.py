533dcbb52f395071dd531d44c04575c4d0a02266
popcorn9499 <popcorn9499@gmail.com>
2018-01-14 20:52:40 -0600

    Stuff I want
    This is just a list of things im planning and some laying out to add some planning that I didnt do in the first few versions

---
+++ utils/Object.py
@@ -0,0 +1,96 @@
+class Object():
+    def __init__(self, attributes:dict):
+        self.__dict__ = attributes
+
+
+
+
+#me playing around code
+t = Object({"test":1,"why":5})
+print(t.why)
+
+
+#things I will need to layout
+#this was mostly gotten off a look at the discord py documenation
+
+'''
+Messages:
+-timestamp
+-edited_timestamp?
+-tts?
+-type?
+-author #this would give the member or user information depending on variables
+-content
+-embeds
+-server
+
+-mentions_everyone?
+
+
+
+
+Roles: #ranks etc
+-id
+-name
+-permissions
+-server
+-position
+-managed?
+-mentionable
+-created_at
+-mention?
+
+
+-Roles: #option
+-list all roles and in which order
+
+
+Server:
+-name
+-emojis
+-members
+-channels
+-icon
+-id
+-owner
+-unavaliable? #basically would stop the bot from doing anything if the server cant be used
+-large? #probably unnessisary
+-default_channel
+-created_at
+-role hierarchy
+-get_channel
+-get_member
+
+
+
+
+Channel:
+-name
+-id
+-server
+-topic
+-is_private
+-position
+-type: voice or text private or group or category
+-is_default?
+-created_at
+
+
+
+Member:? 
+
+User:
+-name
+-id
+-discrimitantor
+-avatar
+-bot #bool if its bot or not
+-mention
+-display_name
+
+
+
+
+Private Channel:?
+
+'''
