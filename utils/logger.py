c56c320aed645cb5364444f63724bee3c7ea1ed0
popcorn9499 <popcorn9499@gmail.com>
2018-04-07 20:09:11 -0500

    Added comments to the logger

--- utils/logger.py
+++ utils/logger.py
@@ -3,29 +3,25 @@ import time
 import os
 
 class logs:
-    def __init__(self,name):
-        print("ya?")
+    def __init__(self,name): #creates the logger object
         self.logger = logging.getLogger(name)
-        formatter = logging.Formatter('%(asctime)s [Thread/%(threadName)s] - %(name)s - %(levelname)s - %(message)s')
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
-        ch.setFormatter(formatter)
+        formatter = logging.Formatter('%(asctime)s [Thread/%(threadName)s] - %(name)s - %(levelname)s - %(message)s') #sets formatting for the console output
+        terminal = logging.StreamHandler()
+        terminal.setLevel(logging.DEBUG)#sets the debug level
+        terminal.setFormatter(formatter)
         self.logger.setLevel(logging.DEBUG)
-        self.logger.addHandler(ch)
-        self.logger.addHandler(LogFile())
+        self.logger.addHandler(terminal) #adds the console handler
+        self.logger.addHandler(LogFile())#adds the log file handler
 
 
 class LogFile(logging.Handler):
-    def emit(self, record):
-        print("ran")
-        print(record.__dict__)
+    def emit(self, record):#creates the log file with whats required
         self.logFile("Log.log","{0} [Thread/{1}] - {2} - {3} - {4} ".format(record.asctime,record.threadName,record.name,record.levelname,record.message))
 
-    def logFile(self,file,msg):
+    def logFile(self,file,msg): #creates the file with what was specified to be put into it
         current_time = time.time()
-
-        if os.path.isfile(file) and library.creationTime(current_time,file) >= (3600*24): 
+        if os.path.isfile(file) and library.creationTime(current_time,file) >= (3600*24):
                 os.rename(file, file + ".1")
-        with open(file, 'a') as f:
-            f.write(msg + "\r\n")
+        with open(file, 'a') as f:#'a' allows us to append a new line to the file
+            f.write(msg + "\r\n") #writes the line to the file
 
