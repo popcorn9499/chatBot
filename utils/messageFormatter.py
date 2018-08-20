7ab3307ca10552392b748bb44babcf14cbb78bdd
popcorn9499 <popcorn9499@gmail.com>
2018-08-19 22:31:12 -0500

    Realised a bug
    If the format you required was on the message contents itself then it wouldnt recognize it in the if statement

--- utils/messageFormatter.py
+++ utils/messageFormatter.py
@@ -7,12 +7,12 @@ async def formatter(unformatMsg):
     formatting = "%authorName% %message%"
     formatting = fileIO.loadConf("config{0}ChatFormatting{0}default.json")["Format"]
     for items in formatting.split(" "): #cycles through all the items and replaces the code name with the contents the message should have
+        items = await removeChar("[",items)
+        items = await removeChar("]",items)
+        items = await removeChar(":",items)
         if items == "%message%":
             formatting = formatting.replace(items,unformatMsg.Message.Contents)
         else:
-            items = await removeChar("[",items)
-            items = await removeChar("]",items)
-            items = await removeChar(":",items)
             formatting = formatting.replace(items,unformatMsg.FormattingOptions[items])
     return formatting
 
