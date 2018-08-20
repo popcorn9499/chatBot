48e83a969299ee9a99fa62a5486ff7576e1f7cd8
popcorn9499 <popcorn9499@gmail.com>
2018-08-19 22:29:28 -0500

    Updated formatting
    Added the abilty to use characters like : and [ ] in the formatting to add something to messages

--- utils/messageFormatter.py
+++ utils/messageFormatter.py
@@ -1,10 +1,21 @@
+import os
+from utils import fileIO
+
+
+
 async def formatter(unformatMsg):
     formatting = "%authorName% %message%"
+    formatting = fileIO.loadConf("config{0}ChatFormatting{0}default.json")["Format"]
     for items in formatting.split(" "): #cycles through all the items and replaces the code name with the contents the message should have
-        print(items)
         if items == "%message%":
-            string = string.replace(items,unformatMsg.Message.Contents)
+            formatting = formatting.replace(items,unformatMsg.Message.Contents)
         else:
-            string = string.replace(items,unformatMsg.FormattingOptions[items])
-    return string
+            items = await removeChar("[",items)
+            items = await removeChar("]",items)
+            items = await removeChar(":",items)
+            formatting = formatting.replace(items,unformatMsg.FormattingOptions[items])
+    return formatting
+
+async def removeChar(char,string):
+    return string.replace(char,"")
 
