4b966427b183a71c7dc924425901aad6f8f9f98e
popcorn9499 <popcorn9499@gmail.com>
2019-02-01 22:46:10 -0600

    started work towards the message filtering
    This is basically nothing completed.. basically me saving before i fall asleep

--- modules/messageFilter.py
+++ modules/messageFilter.py
@@ -12,9 +12,14 @@ class messageFilter:
         self.l = logger.logs("messageFilter")
         fileIO.checkFolder("config{0}messageFilter{0}".format(os.sep),"messageFilter",self.l)
         fileIO.checkFile("config-example{0}messageFilter{0}config.json".format(os.sep),"config{0}messageFilter{0}config.json".format(os.sep),"config.json",self.l)
+        self.filter = fileIO.loadConf("config{0}messageFilter{0}config.json")["Message Filter"]
         pass
 
-    async def filterMessage
+    async def filterMessage(self,message):
+        contents = message.Message.Contents
+        for i in self.filter:
+            if (contents.find(i) == -1)
+                return None
 
 
 messageFilter = messageFilter()
