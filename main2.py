03b5828dd0fdd3ddd1ee3f3fb3eb6f388d68a4ac
popcorn <popcorn9499@gmail.com>
2019-05-19 09:45:16 -0500

    added console
    mental note please please please make this actually setup properly instead of me adding modules to import....

--- main2.py
+++ main2.py
@@ -6,9 +6,11 @@ from sites import youtube
 from sites import irc
 from utils import logger
 from utils import deleteMsg
+from modules import console #move the location of this please... it must start closer to the beginning....
 from modules import chatbot
 from modules import chatLog
 from modules import command
+
 
 async def messageTest(Message):
     print(Message.Contents)
