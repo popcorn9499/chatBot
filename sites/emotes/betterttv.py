72ba3e14ba455661031ed3640e5c4d91d792cebc
popcorn9499 <popcorn9499@gmail.com>
2019-08-10 23:45:58 -0500

    base for betterttv

---
+++ sites/emotes/betterttv.py
@@ -0,0 +1,11 @@
+from sites.emotes.emotes import emotes
+
+class betterttv(enotes):
+    def __init__(self):
+
+        globalUrl = "https://api.betterttv.net/emotes"
+        channelUrlFormat = "https://api.betterttv.net/2/channels/" #:channel
+        super(globalUrl,channelUrlFormat)
+
+    
+    
