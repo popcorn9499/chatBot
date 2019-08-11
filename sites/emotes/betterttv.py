42ddc1379745de1e806b66740d57d761cd0a5b71
popcorn9499 <popcorn9499@gmail.com>
2019-08-10 23:47:33 -0500

    parses globalEmoteData into standardized format

--- sites/emotes/betterttv.py
+++ sites/emotes/betterttv.py
@@ -8,6 +8,15 @@ class betterttv(enotes):
         super(globalUrl,channelUrlFormat)
 
     
+    async def parseGlobalEmoteData(self,emoteList):
+        emoteReturn = {} #this should be in {emoteName: emoteUrl} format
+        for emoteData in emoteList:
+            emoteName = emoteData["regex"]
+            emoteUrl = "https:" + emoteData["url"]
+            emoteUrl = emoteUrl.replace("/1x", "/3x")
+            emoteReturn.update({emoteName: emoteUrl})
+        return emoteReturn
+
     async def getEmote():
         pass
     
