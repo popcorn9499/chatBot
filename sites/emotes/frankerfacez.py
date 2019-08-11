ef60f6c9ba61e2bb1e6ba4a839b0f82b8262069b
popcorn9499 <popcorn9499@gmail.com>
2019-08-11 09:19:41 -0500

    basic ffz foundational data

--- sites/emotes/frankerfacez.py
+++ sites/emotes/frankerfacez.py
@@ -1,4 +1,12 @@
+from sites.emotes.emotes import emotes
+import asyncio
 
+class frankerfacez(enotes):
+    def __init__(self):
+        globalUrl = "https://api.frankerfacez.com/v1/set/global"
+        channelUrlFormat = "https://api.frankerfacez.com/v1/room/" #:channel
+        super(globalUrl,channelUrlFormat)
+        super.loop.create_task(super.updateData(super.globalUrl,"global", self.parseGlobalEmoteData))
     async def _getFrankerFacesEmotesURL(self,emoteUrlList):
         emoteURL = ""
         for key,val in emoteUrlList.items():
