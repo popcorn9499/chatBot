132d45fe67f6695b4985fefd86e8bbec62fd42aa
popcorn9499 <popcorn9499@gmail.com>
2019-08-12 00:37:13 -0500

    decoupled discordpy from the start of the event loop

--- main2.py
+++ main2.py
@@ -3,9 +3,12 @@ from utils.EventHook import EventHook
 import utils
 import modules
 import sites
+import asyncio
 from sites import emotes
 
 #this is the starting point for all the bot tasks
 discordP = sites.discord.Discord()
-discordP.start(utils.config.c.discordToken)
 
+loop = asyncio.get_event_loop()
+loop.create_task(discordP.start(utils.config.c.discordToken))
+loop.run_forever()
